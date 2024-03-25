7a34b3e75c52d674df0b64f12a71c7fec25b9abe
Felix Man <felixwhman@gmail.com>
2024-03-25 15:57:01 -0400

    Updated url endpoints

--- backend/tickit/serializers.py
+++ backend/tickit/serializers.py
@@ -1,24 +1,27 @@
 from rest_framework import serializers
 from .models import Venue, Event
 
+
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     venue = serializers.HyperlinkedRelatedField(
-        view_name = 'venue_detail',
-        read_only = True
+        view_name='venue_detail',
+        read_only=True
     )
     venue_id = serializers.PrimaryKeyRelatedField(
-        queryset = Venue.objects.all(),
-        source = 'venue'
+        queryset=Venue.objects.all(),
+        source='venue'
     )
-    
+
     class Meta:
         model = Event
-        fields = ('venue', 'event_name', 'event_type', 'event_host', 'event_time', 'event_date', 'event_img', 'ticket_price_min', 'ticket_price_max')
+        fields = ('venue_id', 'venue', 'event_name', 'event_type', 'event_host',
+                  'event_time', 'event_date', 'event_img', 'ticket_price_min', 'ticket_price_max')
+
 
 class VenueSerializer(serializers.HyperlinkedModelSerializer):
     event = EventSerializer(
-        many = True,
-        read_only = True
+        many=True,
+        read_only=True
     )
     event_url = serializers.ModelSerializer.serializer_url_field(
         view_name='event_detail'
@@ -26,4 +29,5 @@ class VenueSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Venue
-        fields = ('venue_name', 'street_address', 'city', 'state', 'zip_code', 'venue_type', 'venue_img')
+        fields = ('event', 'event_url', 'venue_name', 'street_address', 'city',
+                  'state', 'zip_code', 'venue_type', 'venue_img')
