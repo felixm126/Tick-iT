3158eafd0de7b064f05c092fd0b8ab49f7c4c088
Felix Man <105676379+felixm126@users.noreply.github.com>
2024-03-25 15:29:33 -0400

    Revert "Added models, serializers, admin, urls, & views"

--- backend/tickit/views.py
+++ backend/tickit/views.py
@@ -1,19 +1,3 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import VenueSerializer, EventSerializer
-from .models import Venue, Event
 
 # Create your views here.
-class VenueList(generics.ListCreateAPIView): # CRUD: view, add
-    queryset = Venue.objects.all()
-    serializer_class = VenueSerializer
-class VenueDetail(generics.RetrieveUpdateDestroyAPIView): # CRUD: view, edit, delete
-    queryset = Venue.objects.all()
-    serializer_class = VenueSerializer
-
-class EventList(generics.ListCreateAPIView): # CRUD: view, add
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
-class EventDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
