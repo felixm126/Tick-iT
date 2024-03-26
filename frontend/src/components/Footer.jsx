bc4ec4806951a9961d2689125e41b9271554f9db
Felix Man <felixwhman@gmail.com>
2024-03-26 15:37:38 -0400

    Footer functionality completed Deleted contactuspage

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,34 @@
+import GitHubButton from 'react-github-btn'
+
 function Footer() {
-	return <></>
+	return (
+		<footer className="github">
+			<div>
+				<h4>Tikit</h4>
+				<GitHubButton href="https://github.com/felixm126/Tick-iT"></GitHubButton>
+			</div>
+			<div>
+				<h4>Felix Man</h4>
+				<>
+					<GitHubButton href="https://github.com/felixm126"></GitHubButton>
+					<a href="https://www.linkedin.com/in/felix-man/">LinkedIn</a>
+				</>
+			</div>
+			<div>
+				<h4>Maddie Maz</h4>
+				<>
+					<GitHubButton href="https://github.com/maddiemaz"></GitHubButton>
+					<a href="https://www.linkedin.com/in/madeline-maslowsky/">LinkedIn</a>
+				</>
+			</div>
+			<div>
+				<h4>Wil Mckay</h4>
+				<>
+					<GitHubButton href="https://github.com/wilmckay"></GitHubButton>
+					<a href="https://github.com/wilmckay">LinkedIn</a>
+				</>
+			</div>
+		</footer>
+	)
 }
 export default Footer
