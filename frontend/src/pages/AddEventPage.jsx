4908c3befecd36faad6a3bf2abf6264898d49479
Felix Man <felixwhman@gmail.com>
2024-03-27 10:24:05 -0400

    Updated addevent form import

--- frontend/src/pages/AddEventPage.jsx
+++ frontend/src/pages/AddEventPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import AddEventForm from './AddEventForm'
+import AddEventForm from '../components/AddEventForm'
 
 function AddEventPage() {
-    return (
-        <div>
-            <h1>Add Event</h1>
-            <AddEventForm />
-        </div>
-    );
+	return (
+		<div>
+			<h1>Add Event</h1>
+			<AddEventForm />
+		</div>
+	)
 }
 
 export default AddEventPage
