304e098ad4532a67944ca3f81c1f93d6c16bbe2f
Felix Man <felixwhman@gmail.com>
2024-03-27 20:38:01 -0400

    Updated Main.jsx. Updated Upcoming events to have a default city of Philadelphia.

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -3,23 +3,24 @@ import EventList from './EventList'
 import EventDetails from './EventDetails'
 import VenueList from './VenueList'
 import VenueDetails from './VenueDetails'
-import AddEventPage from '../pages/AddEventPage'
+// import AddEventPage from '../pages/AddEventPage'
 import AddEventForm from './AddEventForm'
-// import HomePage from '../pages/HomePage'
+import HomePage from '../pages/HomePage'
+import UpcomingEvents from './UpcomingEvents'
 
 function Main() {
 	return (
 		<main>
 			<Routes>
-				{/* <Route path="/" element={<HomePage />} /> */}
-				<Route path="/" element={<EventList />} />
+				<Route path="/" element={<HomePage />} />
 				<Route path="/events" element={<EventList />} />
 				<Route path="/events/:id" element={<EventDetails />} />
 				<Route path="/venues" element={<VenueList />} />
 				<Route path="/venues/:id" element={<VenueDetails />} />
 				<Route path="/event-details" element={<EventDetails />} />
 				<Route path="/venue-details" element={<VenueDetails />} />
-				<Route path='/addevent' element={<AddEventForm/>}/>
+				<Route path="/addevent" element={<AddEventForm />} />
+				<Route path="/upcomingevents" elemennt={<UpcomingEvents />} />
 			</Routes>
 		</main>
 	)
