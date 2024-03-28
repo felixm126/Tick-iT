8bcc96806063a5978eed304dea803fa2085c0f43
maddiemaz <maddiemaz18@gmail.com>
2024-03-28 10:58:23 -0400

    Venue Details, Venue List, Footer

--- frontend/src/components/VenueList.jsx
+++ frontend/src/components/VenueList.jsx
@@ -4,25 +4,24 @@ import axios from 'axios'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faLocationDot} from '@fortawesome/free-solid-svg-icons'
 
-function EventList() {
-	// let navigate = useNavigate()
+function VenueList() {
+	let navigate = useNavigate()
 
-	// const [events, setEvents] = useState([])
+	const [venues, setVenues] = useState([])
 
-	// useEffect(() => {
-	// 	const getEvents = async() => {
-	// 		const response = await axios.get('http://localhost:8000/events')
-	// 		setEvents(response.data)
-	// 	}
-	// 	getEvents()
-	// }, [])
-	// console.log(events)
+	useEffect(() => {
+		const getVenues = async() => {
+			const response = await axios.get('http://localhost:8000/events')
+			setVenues(response.data)
+		}
+		getVenues()
+	}, [])
 
-	// // Sort by soonest date
+	// Sort by nearest, in theory
 
-	// if (!events) {
-	// 	return <div className="loading">Loading...</div>
-	// } else {
+	if (!venues) {
+		return <div className="loading">Loading...</div>
+	} else {
 		return (
 			<div className="venue-list-page">
 				<div className="list-header">
@@ -34,34 +33,29 @@ function EventList() {
 
 				</div>
 				<div className="list-grid">
-					{/* {events.map((event, id) => ( */}
+					{events.map((venue, id) => (
 						<div className="list-card">
 							<div className="list-card-img-container">
-								<img className="list-card-img"/>
-								{/* <img src={event.event_img} alt={event.event_name} className="list-card-img"/> */}
+								<img src={event.event_img} alt={event.event_name} className="list-card-img"/>
 							</div>
 							<div className="list-card-info">
 								<div className="venue-list-info-primary">
-									<div className="text-title-20">Venue Name</div>
-									<div className="text-body-14-med spacing-10">Venue Space Type</div>
-									{/* <div className="text-title-20">{venue.venue_name}</div>
-									<div className="text-body-14-med spacing-10">{venue.venue_type}</div> */}
+									<div className="text-title-20">{venue.venue_name}</div>
+									<div className="text-body-14-med spacing-10">{venue.venue_type}</div>
 								</div>
 								<div className="venue-list-info-location spacing-5">
-									<div className="text-body-14-light">Venue Street Address</div>
-									<div className="text-body-14-light">City, State, Zip</div>
-									{/* <div className="text-body-14-light">{venue.street_address}</div>
-									<div className="text-body-14-light">{venue.city}, {venue.state} {venue.zip_code}</div> */}
+									<div className="text-body-14-light">{venue.street_address}</div>
+									<div className="text-body-14-light">{venue.city}, {venue.state} {venue.zip_code}</div>
 								</div>
 							</div>
-							<div className="list-card-button-container">
+							<Link className="list-card-button-container" to={`/events/${id + 1}`} key={id}>
 								<div className="list-card-button-text text-stylized-14">view</div>
-							</div>
+							</Link>
 						</div>
-					{/* ))} */}
+					))}
 				</div>
 			</div>
 		)
 	}
-// }
-export default EventList
+}
+export default VenueList
